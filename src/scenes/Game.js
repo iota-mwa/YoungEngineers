611bcf84e2b6b01c4308b20b4bd962d249ddb97e
iota-mwa <james_ching_kian_leng@moe.edu.sg>
2025-08-29 07:43:35 +0800

    Update Game.js

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -60,7 +60,7 @@ class Game extends Phaser.Scene {
 
         // Score display
         this.jiaDeScoreText = this.add.text(100, 50, 'Jia De: 0', { fontSize: '32px', fill: '#fff' });
-        this.opponentScoreText = this.add.text(800, 50, 'Opponent: 0', { fontSize: '32px', fill: '#fff' });
+        this.opponentScoreText = this.add.text(700, 50, 'Opponent: 0', { fontSize: '32px', fill: '#fff' });
 
         // Game over text (hidden initially)
         this.gameOverText = this.add.text(WIDTH / 2, HEIGHT / 2, '', { 
@@ -142,3 +142,4 @@ class Game extends Phaser.Scene {
     }
 
 }
+
